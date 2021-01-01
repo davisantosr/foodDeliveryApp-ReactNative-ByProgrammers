f7c8def6183f021f3075e30aa08f1160400c9537
davisantosr <davi-rocha2@hotmail.com>
2020-12-31 21:44:52 -0300

    add more tabs initial structure

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -37,6 +37,60 @@ const Tabs = () => {
           )
         }}
       />
+      <Tab.Screen
+        name='Search'
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image 
+              source={icons.search}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary
+              }}
+
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Like'
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image 
+              source={icons.like}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary
+              }}
+
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='User'
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image 
+              source={icons.user}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary
+              }}
+
+            />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
