d43723cfc88289e465489a536551c905f45b2702
davisantosr <davi-rocha2@hotmail.com>
2021-01-09 17:12:41 -0300

    restaurant screen|add renderOrder

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -18,6 +18,7 @@ import { icons, COLORS, SIZES, FONTS } from '../constants'
 
 const Restaurant = ({route, navigation}) => {
 
+  const scrollX = new Animated.Value(0)
   const [ restaurant, setRestaurant ] = React.useState(null)
   const [ currentLocation, setCurrentLocation ] = React.useState(null)
 
@@ -105,7 +106,11 @@ const Restaurant = ({route, navigation}) => {
         scrollEventThrottle={16}
         snapToAlignment='center'
         showsHorizontalScrollIndicator={false}
-        // onScroll={}
+        onScroll={Animated.event([
+          {nativeEvent:{contentOffset: {x: scrollX}}}
+        ],
+          {useNativeDriver: false}
+        )}
       >
         
         {
@@ -168,6 +173,7 @@ const Restaurant = ({route, navigation}) => {
                       borderBottomRightRadius: 25,
                     }}
                   >
+                    <Text style={{...FONTS.h2}}>+</Text>
                   </TouchableOpacity>
                 </View>
               </View>
@@ -224,10 +230,152 @@ const Restaurant = ({route, navigation}) => {
     )
   }
 
+  function renderDots() {
+    const dotPosition = Animated.divide(scrollX, SIZES.width)
+    return (
+      <View style={{height: 30}}>
+        <View style={{
+          flexDirection: 'row',
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          height: SIZES.padding
+        }}>
+          {restaurant?.menu.map((item, index) => {
+            const opacity = dotPosition.interpolate({
+              inputRange: [index -1, index, index + 1], 
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp'
+            })
+
+            const dotSize = dotPosition.interpolate({
+              inputRange: [index -1, index, index + 1], 
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp'
+            })
+
+            const dotColor = dotPosition.interpolate({
+              inputRange: [index-1, index, index+1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: 'clamp'
+            })
+            return (
+              <Animated.View 
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius, 
+                  marginHorizontal: 6, 
+                  width: dotSize,
+                  height: dotSize, 
+                  backgroundColor: dotColor
+                }}
+              />     
+            )
+          })}
+        </View>
+      </View>
+    )
+  }
+
+  function renderOrder() {
+    return (
+      <View>
+        {
+          renderDots()
+        }
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            borderTopLeftRadius: 40, 
+            borderTopRightRadius: 40, 
+
+            
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2, 
+              paddingHorizontal: SIZES.padding * 3, 
+              borderBottomColor: COLORS.lightGray2, 
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text style={{...FONTS.h3}}>Items in Cart</Text>
+            <Text style={{...FONTS.h3}}>$ 45</Text>
+          </View>
+
+          <View style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding *2, 
+              paddingHorizontal: SIZES.padding * 3,
+            }}
+          >
+            <View style={{flexDirection: 'row'}}>
+              <Image 
+                source={icons.location}
+                resizeMode='contain'
+                style={{
+                  width: 20, 
+                  height: 20, 
+                  tintColor: COLORS.darkgray
+                }}
+              />
+              <Text style={{marginLeft: SIZES.padding, ...FONTS.h4}}>
+                Location
+              </Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Image 
+                source={icons.master_card}
+                resizeMode={'contain'}
+                style={{
+                  width: 20, 
+                  height: 20, 
+                  tintColor: COLORS.darkgray
+                }}
+              />
+              <Text style={{marginLeft: SIZES.padding, ...FONTS.h4}}>99999</Text>
+            </View>
+                        
+          </View>
+
+          {/* Order Button */}
+          <View
+            style={{
+              padding: SIZES.padding * 2, 
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+          <TouchableOpacity
+            style={{
+              width: SIZES.width * 0.9, 
+              padding: SIZES.padding, 
+              backgroundColor: COLORS.primary,
+              alignItems: 'center',
+              borderRadius: SIZES.radius,
+            }}
+          >
+            <Text style={{color: COLORS.white, ...FONTS.h2}}>
+              Order
+            </Text>
+          </TouchableOpacity>
+          </View>
+         
+        </View>
+      </View>
+      
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container} >
       {renderHeader()}
       {renderFoodInfo()}
+      {renderOrder()}
 
     </SafeAreaView>
   )
